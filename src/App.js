c367551d677d3b8161e9d9db919f69d41190b8dd
Joe <105171436+cisjoe@users.noreply.github.com>
2024-07-31 03:19:26 +0300

    Fix Github Route

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route path="Van-Life" index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="vans" element={<Vans />} />
             <Route path="vans/:id" element={<VanDetail />} />
